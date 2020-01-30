085dfc6477721132c18b419f362e42f08bceac69
eurioh <euri.elliot.oh@gmail.com>
2020-01-30 17:54:30 -0500

    add submit and ejs

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ mongoose.set('useCreateIndex', true); //use for third party library
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret:String
 });
 
 userSchema.plugin(passportLocalMongoose);//set up user schema to use passortlocal as a plugin
@@ -137,20 +138,50 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/secrets", function(req,res){
+    // if(req.isAuthenticated()){
+    //     res.render("secrets");
+    // }else{
+    //     res.redirect("/login");
+    // }
+
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUsers){
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    });
+});
+
+app.get("/submit", function(req,res){
     if(req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     }else{
         res.redirect("/login");
     }
 });
 
-// app.get("/submit", function(req,res){
-//     if(req.isAuthenticated()){
-//         res.render("submit");
-//     }else{
-//         res.redirect("/login");
-//     }
-// });
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
+    console.log(req.user.id);
+
+    User.findById(req.user.id, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    }); 
+});
+
+
 
 app.post("/register", function (req, res) {
     //This method comes from passportlocalmongoose package
